cf8b59e07cd2075502ad9135e704aefa00144404
Angel <angelglezz.7@gmail.com>
2019-09-23 11:29:25 -0500

    test desk

--- js/page.js
+++ js/page.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         /* Check the location of each desired element */
         $('.hidden').each(function(i){
             
-            var bottom_of_object = ($(this).offset().top + $(this).outerHeight())/3;
+            var bottom_of_object = ($(this).offset().top + $(this).outerHeight());
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
